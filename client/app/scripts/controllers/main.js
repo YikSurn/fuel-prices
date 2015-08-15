96d75200e95145793b15a64687a8b4ac99ce68c6
Angeline <angeline07561@gmail.com>
2015-08-15 21:44:50 +1000

    Add remaining front end files

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -15,10 +15,49 @@ angular.module('fuelPricesApp')
     var mapCanvas = document.getElementById('googlemap');
     var mapOptions = {
       center: {lat: -37.814107, lng: 144.96328},
-      zoom: 16,
+      zoom: 10,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var initialLocation;
+    var browserSupportFlag = new Boolean();
+    var infoWindow = new google.maps.InfoWindow({map: map})
+    // Try W3C Geolocation
+    if(navigator.geolocation) {
+      browserSupportFlag = true;
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Your location');
+        map.setCenter(pos);
+        // initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+        // map.setCenter(initialLocation);
+      }, function() {
+        handleNoGeolocation(browserSupportFlag);
+      });
+    }
+    // Browser doesn't support Geolocation
+    else {
+      browserSupportFlag = false;
+      handleNoGeolocation(browserSupportFlag);
+    }
+
+    // No Geolocation handling
+    function handleNoGeolocation(errorFlag) {
+      if (errorFlag == true) {
+        alert("Couldn't Find Your Location!");
+        initialLocation = new google.maps.LatLng(-37.82396, 144.99097)
+      }
+      else {
+        alert("Your browser doesn't support Geolocation. We've placed you at Inspire 9");
+        initialLocation = new google.maps.LatLng(-37.82396, 144.99097)
+      }
+      map.setCenter(initialLocation);
+    }
 
     $http.get(API_MOUNT + 'station')
       .success(function(data) { 
