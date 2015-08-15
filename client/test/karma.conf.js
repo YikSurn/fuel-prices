e70add296f3a2c128ccb39995d177ac2ceb7ed29
Yik Surn Chong <yiksurn.chong@gmail.com>
2015-08-15 17:25:44 +1000

    Comma error

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -30,7 +30,6 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/bootstrap/dist/js/bootstrap.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
