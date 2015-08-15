a91b5ec5ea7f5bfc77aea179f93c7ec12b5566db
Yik Surn Chong <yiksurn.chong@gmail.com>
2015-08-15 20:41:59 +1000

    Infowindow for points on google map

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -12,41 +12,48 @@ angular.module('fuelPricesApp')
 
     $scope.stations;
 
+    var infowindow;
     var mapCanvas = document.getElementById('googlemap');
     var mapOptions = {
       center: {lat: -37.814107, lng: 144.96328},
       zoom: 8,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var gMap = new google.maps.Map(mapCanvas, mapOptions);
 
     $http.get(API_MOUNT + 'station')
       .success(function(data) { 
         $scope.stations = data;
 
         for (var i = 0; i < $scope.stations.length; i++) {
+          temp_station = angular.copy($scope.stations[i]);
+          console.log(temp_station);
+
+          $scope.stations[i]['address'] = temp_station.street + ', ' + temp_station.suburbs + temp_station.postcode
 
           $scope.stations[i]['marker'] = new google.maps.Marker({
-            position: {lat: $scope.stations[i]['latitude'], lng: $scope.stations[i]['latitude']},
-            map: map,
+            position: {lat: parseFloat(temp_station['latitude']), lng: parseFloat(temp_station['latitude'])},
+            map: gMap,
             animation: google.maps.Animation.DROP,
-            title: $scope.stations[i]['name']
+            title: temp_station['name']
             // label: "a"
           });
-
-          $scope.stations[i]['marker'].addListener('click', showDetails);
+          attachDetailsAndWindow($scope.stations[i]['marker'], $scope.stations[i]);
         };
       });
-
-    var infowindow = new google.maps.InfoWindow({
-      content: "HELLO NEXUSFUEL!"
-    });
 
     // google.maps.event.addDomListener(window, 'load', initMap);
     // marker.setMap(map);
 
-    function showDetails() {
-      infowindow.open(map, marker);
+    function attachDetailsAndWindow(marker, station) {
+      var infowindow = new google.maps.InfoWindow({
+        content: 'Station Name: ' + station.name +
+          'Address: ' + station.address + station.
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(marker.get('map'), marker);
+      });
     };
 
   });
